65efd6bc1ff3937e992619f37efafb5068334622
EUR\SESA455926 <CAMILLE.VIOT@non.schneider-electric.com>
2017-10-13 13:36:52 +0200

    partly fixing .bat files

--- make-ponygame-example.bat
+++ make-ponygame-example.bat
@@ -4,15 +4,17 @@ pushd %~dp0
 ECHO Compiling pony-game...
 REM Link to Windows Kits and native libraries.
 REM http://tutorial.ponylang.org/appendices/compiler-args.html
+
+COPY /Y "Bin\PonyGameNative\x64\Debug\PonyGameNative.lib" "Source\PonyGameNative.lib"
 ponyc -p "Bin\x64" -p "Source" -o Bin\PonyGameExample Source\PonyGameExample
 
 ECHO Copying native libs...
-COPY "Bin\x64\PonyGameNative.dll" "Bin\PonyGameExample"
+COPY /Y "Bin\x64\Debug\PonyGameNative.dll" "Bin\PonyGameExample"
 
 ECHO Copying resources...
-COPY "Source\PonyGame\*.ini" "Bin\PonyGameExample"
-COPY "Source\PonyGameExample\*.png" "Bin\PonyGameExample"
-COPY "Source\PonyGameExample\*.ini" "Bin\PonyGameExample"
+COPY /Y "Source\PonyGame\*.ini" "Bin\PonyGameExample"
+COPY /Y "Source\PonyGameExample\*.png" "Bin\PonyGameExample"
+COPY /Y "Source\PonyGameExample\*.ini" "Bin\PonyGameExample"
 
 PAUSE
 popd
--- run-ponygame-example.bat
+++ run-ponygame-example.bat
@@ -1,6 +1,8 @@
+pushd %~dp0
 @ECHO OFF
 
 CD Bin\PonyGameExample
 DEL *.log
 PonyGameExample.exe
 ponygame.log
+popd
